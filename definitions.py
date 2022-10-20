69b5543ae769e1b25ec59458bef86f3630627d7e
qbon <ostatek.jakub98@gmail.com>
2022-10-20 22:29:11 +0200

    Restructered and add poetry

--- definitions.py
+++
@@ -1,5 +0,0 @@
-import os
-from pathlib import Path
-
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-CONFIG_PATH = Path(os.path.join(ROOT_DIR, "config.toml"))
---
+++ modbus_simulator/definitions.py
@@ -0,0 +1,5 @@
+import os
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parents[1]
+CONFIG_PATH = ROOT_DIR / "config.toml"
